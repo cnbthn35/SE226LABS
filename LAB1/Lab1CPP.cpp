fea918ab857b07fb7a9dd95632ac2efeca09395c
cnbthn35 <cnbthn735@gmail.com>
2025-02-20 20:56:26 +0300

    LAB1

---
+++ LAB1/Lab1CPP.cpp
@@ -0,0 +1,89 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name, id;
+
+    cout<<"Please do not leave any spaces when entering.";
+
+    cout<<"What is your name?"<<endl;
+    cin>>name;
+    cout<<"Hello " + name <<endl;
+    cout<<"What is your student ID?"<<endl;
+    cin>>id;
+    cout<<"Your ID is " + id <<endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int var1, var2;
+    cout<<"Please do not leave any spaces when entering";
+    cout<<"Please enter var1: ";
+    cin>>var1;
+    cout<<"Please enter var2: ";
+    cin>>var2;
+    int sum, diff, prod;
+    sum = var1 + var2;
+    diff = var1 - var2;
+    prod = var1 * var2;
+    cout<<"var1: "<<var1<<endl;
+    cout<<"var2: "<<var2<<endl;
+    cout<<"sum: "<<sum<<endl;
+    cout<<"diff: "<<diff<<endl;
+    cout<<"prod: "<<prod<<endl;
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name, surname;
+    float labgrade, midtermgrade, finalgrade;
+
+    cout<<"Please do not leave any spaces when entering.";
+    cout<<"What is your name? ";
+    cin>>name;
+    cout<<"What is yoru surname? ";
+    cin>>surname;
+
+    cout<<"What is your lab grade? ";
+    cin>>labgrade;
+    cout<<"What is your midterm grade? ";
+    cin>>midtermgrade;
+    cout<<"What is your final grade? ";
+    cin>>finalgrade;
+
+    float lastscore;
+    lastscore = (labgrade * 25 / 100) + (midtermgrade * 35 / 100) + (finalgrade * 40 / 100);
+
+    cout<<"Name: "<<name + " " + surname<<endl;
+    cout<<"Lab: "<<labgrade<<endl;
+    cout<<"Midterm: "<<midtermgrade<<endl;
+    cout<<"Final: "<<finalgrade<<endl;
+    cout<<"Last score: "<<lastscore<<endl;
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"*\n**\n***\n**\n*"<<endl;
+    return 0;
+}
+*/
---
+++ LAB1/Lab1Python.py
@@ -0,0 +1,41 @@
+'''
+print("What is your name ?")
+x = input("Name:")
+print("Hello "+ x)
+print("What is your school id")
+y= input("Id:")
+print("Your id is: " + y)
+'''
+
+'''
+var1 = int(input(' First value'))
+var2 = int(input(' Second value'))
+sum = var1+var2
+diff = var1-var2
+prod = var1*var2
+print("Sum = " + str(sum))
+print("Diff = " + str(diff))
+print("Prod = " + str(prod))
+'''
+
+'''
+print("What is your name ?")
+name = input("Name:")
+print("What is your lab grade")
+lab = int(input('Lab:'))
+print("What is your midterm grade")
+mid = int(input('Midterm:'))
+print("What is your final grade")
+final = int(input('Final:'))
+print("Name: " + name)
+print("Lab = " + str(lab))
+print("Midterm = " + str(mid))
+print("Final = " + str(final))
+score = (lab*(25/100))+(mid*(35/100))+(final*(40/100))
+print("Score = " + str(score))
+'''
+
+'''
+print('*\n**\n***\n**\n*')
+'''
+
